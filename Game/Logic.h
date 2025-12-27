ed937a95f2497ddfd7d2e994f2f4e7f3421c5311
Даниил Глушков <mersik1333@bk.ru>
2025-12-27 14:32:08 +0300

    Реализованы функции find_best_turns(), find_first_best_turn() и find_best_turns_rec()

--- Game/Logic.h
+++ Game/Logic.h
@@ -16,32 +16,109 @@ class Logic
         optimization = (*config)("Bot", "Optimization");
     }
 
-    vector<move_pos> find_best_turns(const bool color);
+    vector<move_pos> find_best_turns(const bool color) {
+        next_move.clear();
+        next_best_state.clear();
 
-    // Выполняет ход на копии матрицы доски. 
-        // Алгоритм: 
-        // 1. Если ход является ударом (xb != -1) — снимаем побитую шашку. 
-        // 2. Проверяем, превращается ли шашка в дамку:
-        // - белая (1) становится дамкой (3), если дошла до 0-й строки. 
-        // - чёрная (2) становится дамкой (4), если дошла до 7-й строки. 
-        // 3. Перемещаем шашку на новую позицию. 
-        // 4. Освобождаем старую клетку. 
-        // Возвращает: новую матрицу после хода.
-    vector<vector<POS_T>> make_turn(vector<vector<POS_T>> mtx, move_pos turn) const
-    {
-        // Если xb != -1 — это удар, удаляем побитую шашку
-        if (turn.xb != -1)
-            mtx[turn.xb][turn.yb] = 0;
-        // Проверка превращения в дамку 
-        // 1 → 3 (белая → белая дамка), если дошла до верхней строки 
-        // 2 → 4 (чёрная → чёрная дамка), если дошла до нижней строки
-        if ((mtx[turn.x][turn.y] == 1 && turn.x2 == 0) || (mtx[turn.x][turn.y] == 2 && turn.x2 == 7))
-            mtx[turn.x][turn.y] += 2;
-        // Перемещаем шашку на новую клетку
-        mtx[turn.x2][turn.y2] = mtx[turn.x][turn.y];
-        // Освобождаем старую клетку
-        mtx[turn.x][turn.y] = 0;
-        return mtx;
+        find_first_best_turn(board->get_board(), color, -1, -1, 0);
+
+        vector<move_pos> res;
+        int state = 0;
+        do
+        {
+           res.push_back(next_move[state]);
+           state = next_best_state[state];
+        } 
+        while (state != -1 && next_move[state].x != -1);
+        return res;
+    }
+
+   private:
+    double find_first_best_turn(vector<vector<POS_T>> mtx, const bool color, const POS_T x, const POS_T y, size_t state,
+        double alpha = -1) {
+        next_move.emplace_back(-1, -1, -1, -1);
+        next_best_state.push_back(-1);
+        if (state !=0)
+        {
+            find_turns(x, y, mtx);
+        }
+        auto now_turns = turns;
+        auto now_have_beats = have_beats;
+
+        if (!now_have_beats && state != 0)
+        {
+            return find_best_turns_rec(mtx, 1 - color, 0, alpha);
+        }
+        double best_score = -1;
+        for (auto turn : now_turns) {
+            size_t new_state = next_move.size();
+            double score;
+            if (now_have_beats) {
+                score =  find_first_best_turn(make_turn(mtx, turn), color, turn.x2, turn.y2, new_state, best_score);
+            }
+            else {
+                score = find_best_turns_rec(make_turn(mtx, turn), 1 - color, 0, best_score);
+            }
+            if (score > best_score) {
+                best_score = score;
+                next_move[state] = turn;
+                next_best_state[state] = (now_have_beats ? new_state : -1);
+            }
+        }
+        return best_score;
+    }
+
+    double find_best_turns_rec(vector<vector<POS_T>> mtx, const bool color, const size_t depth, double alpha = -1,
+        double beta = INF + 1, const POS_T x = -1, const POS_T y = -1) {
+        if (depth == Max_depth) {
+            return calc_score(mtx, (depth % 2 == color));
+        }
+        if (x != -1) {
+            find_turns(x, y, mtx);
+        }
+        else
+        {
+            find_turns(color, mtx);
+        }
+        auto now_turns = turns;
+        auto now_have_beats = have_beats;
+        if (!now_have_beats && x != -1) {
+            return find_best_turns_rec(mtx, 1 - color, depth + 1, alpha, beta);
+        }
+
+        if (turns.empty()) {
+            return (depth % 2 ? 0 : INF);
+        }
+
+        double min_score = INF + 1;
+        double max_score = - 1;
+        for (auto turn : now_turns) {
+            double score;
+            if (now_have_beats) {
+                score = find_best_turns_rec(make_turn(mtx, turn), color, depth, alpha, beta, turn.x2, turn.y2);
+            }
+            else
+            {
+                score = find_best_turns_rec(make_turn(mtx, turn), 1 - color, depth + 1, alpha, beta);
+            }
+            min_score = min(min_score, score);
+            max_score = max(max_score, score);
+
+            if (depth % 2)
+            {
+                alpha = max(alpha, max_score);
+            }
+            else {
+                beta = min(beta, min_score);
+            }
+            if (optimization != "O0" && alpha > beta) {
+                break;
+            }
+            if (optimization == "O2" && alpha == beta) {
+                return (depth % 2 ? max_score +1 : min_score - 1);
+            }
+        }
+        return (depth % 2 ? max_score : min_score);
     }
 
     // Оценивает текущее состояние доски для бота. 
@@ -91,13 +168,31 @@ class Logic
         return (b + bq * q_coef) / (w + wq * q_coef);
     }
 
-
-    double find_first_best_turn(vector<vector<POS_T>> mtx, const bool color, const POS_T x, const POS_T y, size_t state,
-                             double alpha = -1);
-
-    double find_best_turns_rec(vector<vector<POS_T>> mtx, const bool color, const size_t depth, double alpha = -1,
-                            double beta = INF + 1, const POS_T x = -1, const POS_T y = -1);
-    
+    // Выполняет ход на копии матрицы доски. 
+        // Алгоритм: 
+        // 1. Если ход является ударом (xb != -1) — снимаем побитую шашку. 
+        // 2. Проверяем, превращается ли шашка в дамку:
+        // - белая (1) становится дамкой (3), если дошла до 0-й строки. 
+        // - чёрная (2) становится дамкой (4), если дошла до 7-й строки. 
+        // 3. Перемещаем шашку на новую позицию. 
+        // 4. Освобождаем старую клетку. 
+        // Возвращает: новую матрицу после хода.
+    vector<vector<POS_T>> make_turn(vector<vector<POS_T>> mtx, move_pos turn) const
+    {
+        // Если xb != -1 — это удар, удаляем побитую шашку
+        if (turn.xb != -1)
+            mtx[turn.xb][turn.yb] = 0;
+        // Проверка превращения в дамку 
+        // 1 → 3 (белая → белая дамка), если дошла до верхней строки 
+        // 2 → 4 (чёрная → чёрная дамка), если дошла до нижней строки
+        if ((mtx[turn.x][turn.y] == 1 && turn.x2 == 0) || (mtx[turn.x][turn.y] == 2 && turn.x2 == 7))
+            mtx[turn.x][turn.y] += 2;
+        // Перемещаем шашку на новую клетку
+        mtx[turn.x2][turn.y2] = mtx[turn.x][turn.y];
+        // Освобождаем старую клетку
+        mtx[turn.x][turn.y] = 0;
+        return mtx;
+    }
 
 public:
     // Находит все возможные ходы для заданного цвета. 
